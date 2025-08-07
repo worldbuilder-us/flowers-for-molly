7df93bdac4bda2d68ec27608ef73bcf364550e80
Shirish Sarkar <sarkar.shirish@gmail.com>
2025-08-06 17:29:44 -0700

    nextjs init

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="text-4xl font-bold">Flowers For Molly</h1>
+        <p className="text-lg text-gray-700">
+          A generative art piece serving to commemorate the life of Molly Bird.
+        </p>
+      </main>
+
+    </div>
+  );
+}
