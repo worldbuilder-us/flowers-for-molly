1b0021e8e01328e2262cfe39d3e3f99f7519f8cd
Shirish Sarkar <sarkar.shirish@gmail.com>
2025-11-22 14:47:48 -0800

    font imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 // src/app/layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css"; // ensure html/body heights set here
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
