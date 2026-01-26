ef7b7af9288d6a6c70f598e50fad1fac855b8b11
Shirish Sarkar <sarkar.shirish@gmail.com>
2026-01-26 02:48:54 -0800

    forest wip

--- src/app/garden/biomeRegistry.ts
+++ src/app/garden/biomeRegistry.ts
@@ -1,7 +1,15 @@
 // src/app/garden/biomeRegistry.ts
 import type { BiomeManifest } from "./biomeManifest";
 import meadow from "./manifests/meadow.json";
+import meadowToForest from "./manifests/meadow_to_forest.json";
+import forest from "./manifests/forest.json";
+import forestToMeadow from "./manifests/forest_to_meadow.json";
 
-export const BIOME_MANIFESTS: BiomeManifest[] = [meadow];
+export const BIOME_MANIFESTS: BiomeManifest[] = [
+  meadow,
+  meadowToForest,
+  forest,
+  forestToMeadow,
+];
 
 export const BIOME_ORDER = BIOME_MANIFESTS.map((b) => b.id);
